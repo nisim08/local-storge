822747184ed5ff4ff2942a82c2d7cb7214173da7
nisim08 <nisimeitan08@gmail.com>
2023-01-20 21:10:08 +0200

    the best update

--- מחשבון.js
+++ מחשבון.js
@@ -4,12 +4,47 @@
 let money = 0;
 let bank = 0;
 let eror = 0;
+let hack = 0;
+
+function hackk(){
+    hack = hack + 1;
+    localStorage.setItem("hack", hack);
+erorr
+    if (hack == 5) {
+
+        hack = 0; 
+        money = 0;
+        
+        localStorage.setItem("money", money);
+        localStorage.setItem("hack", hack);
+        document.getElementById("h1").innerHTML = localStorage.getItem("money")
+        document.getElementById("h12").innerHTML = localStorage.getItem("bank")
+        hack = 0; 
+        money = 0;
+     new Audio('df.mp3').play();
+     eror = eror + 1;
+     localStorage.setItem("eror", eror);
+     erorr();
+    
+    }
 
 
+    
 
+}
+
+function erorr() {
+    while (localStorage.getItem("eror") == 5) {
+ alert("get out of here")   
+}
+}
+
+//? whan you prees on despit it wiil add a 10 dolar to your wallet
 document.getElementById("despit").onclick = despit;
 
 function despit(){
+    hackk();
+
     money = money + 10;
     localStorage.setItem("money", money);
     localStorage.setItem("bank", bank)
@@ -23,10 +58,12 @@ function despit(){
 
 document.getElementById("h1").innerHTML = localStorage.getItem("money")
 document.getElementById("h12").innerHTML = localStorage.getItem("bank")
+
 localStorage.setItem("money", money);
 localStorage.setItem("bank", bank);
+
 alert("you can hold up to 100 dolar");
-eror = eror + 1;
+hackk();
 
 
 
@@ -37,30 +74,23 @@ eror = eror + 1;
 
 document.getElementById("windrow").onclick = windrow;
 
-function windrow() {
-    let x = window.prompt("how much do you want to despit")
-    if(money >= x){
-     bank = localStorage.getItem("bank") + x;
-     money = localStorage.getItem("money") - x;
-     
-     localStorage.setItem("money", money);
-     localStorage.setItem("bank", bank);
+function windrow(){
+ {
+    if(money => 10)
+    bank = bank + 10;
+    money = money - 10;
+    localStorage.setItem("money", money);
+    localStorage.setItem("bank", bank)
+    document.getElementById("h1").innerHTML = localStorage.getItem("money")
+    document.getElementById("h12").innerHTML = localStorage.getItem("bank")
 
-     document.getElementById("h1").innerHTML = localStorage.getItem("money")
-     document.getElementById("h12").innerHTML = localStorage.getItem("bank")
+    hack = hack - 1;
+    localStorage.setItem("hack", hack);
+ new Audio('dd.mp3').play();
+   
 
-     eror = eror + 1
-
-     
-}
-
-else {
-    alert("you don't have enough money")
-    eror = eror + 1;
-    
 }
 }
-
 
 
 
@@ -68,4 +98,11 @@ else {
 window.onload = function() {
     document.getElementById("h1").innerHTML = localStorage.getItem("money")
     document.getElementById("h12").innerHTML = localStorage.getItem("bank")
+    
+    
 }
+
+
+
+
+//! hunred lins of code wow
